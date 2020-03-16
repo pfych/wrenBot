6dcc2125df537d205a8e394fbee848c727041da5
Puffycheeses <contact@pfy.ch>
2020-03-16 21:04:27 +1100

    createEmbed own file

--- src/index.js
+++ src/index.js
@@ -1,46 +1,17 @@
 const Discord = require("discord.js")
 const songLink = require("songlink-api")
 
+const createEmbed = require("./functions/createEmbed")
+const config = require("./lib/data")
+
 const client = new Discord.Client()
-
-const platforms = [
-  "open.spotify.com",
-  "play.google.com/music/m",
-  'music.apple.com',
-  'soundcloud.com'
-]
-
-const platformData = {
-  "spotify": {
-    emoji: "686481957730779146",
-    prettyName: "Spotify"
-  },
-  "soundcloud": {
-    emoji: "686481619405766706",
-    prettyName: "Soundcloud"
-  },
-  "google": {
-    emoji: "686482178464284674",
-    prettyName: "Google Play"
-  },
-  "appleMusic": {
-    emoji: "686482030120140886",
-    prettyName: "Apple Music"
-  },
-  "youtube": {
-    emoji: "689044959290196020",
-    prettyName: "Youtube"
-  }
-}
-const zeroWidth = "​"
-let cache = []
 
 client.on("message", async msg => {
   if (msg.author.bot) return;
-  if(new RegExp(platforms.join("|")).test(msg)) {
+  if(new RegExp(config.platforms.join("|")).test(msg)) {
     let services = msg.content.match(/\bhttps?:\/\/\S+/gi)
     services.forEach(async (service) => {
-      let embed = createEmbed(await getSongLink(service))
+      let embed = createEmbed(await getSongLink(service), client)
       msg.channel.send({ embed })
     })
   }
@@ -51,42 +22,17 @@ client.on('ready', () => {
 });
 
 async function getSongLink(url) {
-  for (let i = 0; i < cache.length; ++i) {
-    if (cache[i].url === url) {
+  for (let i = 0; i < config.cache.length; ++i) {
+    if (config.cache[i].url === url) {
       console.log("Returning from cache")
-      return {...cache[i].data, cached: true}
+      return {...config.cache[i].data, cached: true}
     }
   }
 
   let data = await songLink.getLinks({url: url, apiKey: process.env.api})
   console.log("Pushing to cache")
-  cache.push({url: url, data: data})
+  config.cache.push({url: url, data: data})
   return {...data, cached: false}
-}
-
-function createEmbed(data) {
-  console.log(data)
-  const itunesData = data.entitiesByUniqueId[Object.keys(data.entitiesByUniqueId).filter(item => item.includes("SOUNDCLOUD") | item.includes("SPOTIFY") | item.includes("APPLE"))[0]]
-  const linksByPlatform = data.linksByPlatform
-
-  const fieldsArray = Object.keys(linksByPlatform).filter(platform => Object.keys(platformData).includes(platform)).map(_ => {
-    return {
-      "name": `${zeroWidth}`,
-      "value": `${client.emojis.resolve(platformData[_].emoji)} [${platformData[_].prettyName}](${linksByPlatform[_].url})`
-    }
-  })
-
-  return {
-    "title": `${itunesData.title}`,
-    "description": `By ${itunesData.artistName}`,
-    "thumbnail": {
-      "url": `${itunesData.thumbnailUrl}`
-    },
-    "footer": {
-      "text": `Service provided by https://song.link ${data.cached ? `(Cached)` : `(Fetched)`}`
-    },
-    "fields": fieldsArray
-  }
 }
 
 function login() {
---
+++ src/lib/data.js
@@ -0,0 +1,34 @@
+exports.platformData = {
+  "spotify": {
+    emoji: "686481957730779146",
+    prettyName: "Spotify"
+  },
+  "soundcloud": {
+    emoji: "686481619405766706",
+    prettyName: "Soundcloud"
+  },
+  "google": {
+    emoji: "686482178464284674",
+    prettyName: "Google Play"
+  },
+  "appleMusic": {
+    emoji: "686482030120140886",
+    prettyName: "Apple Music"
+  },
+  "youtube": {
+    emoji: "689044959290196020",
+    prettyName: "Youtube"
+  }
+}
+
+exports.platforms = [
+  "open.spotify.com",
+  "play.google.com/music/m",
+  'music.apple.com',
+  'soundcloud.com'
+]
+
+
+exports.zeroWidth = "​"
+
+exports.cache = []
