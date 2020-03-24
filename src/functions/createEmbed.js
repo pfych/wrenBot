1905911bacd024344479ac6972ac066ef48c0d16
Noah <noahheague@gmail.com>
2020-03-24 21:04:30 +1100

    Fixed an issue where the API wouldnt resolve some albums

--- src/functions/createEmbed.js
+++ src/functions/createEmbed.js
@@ -1,7 +1,7 @@
 const config = require("../lib/data")
 
 const createEmbed = (data, client) => {
-  const metadata = data.entitiesByUniqueId[Object.keys(data.entitiesByUniqueId).filter(item => item.includes("SOUNDCLOUD") | item.includes("SPOTIFY") | item.includes("APPLE"))[0]]
+  const metadata = data.entitiesByUniqueId[Object.keys(data.entitiesByUniqueId).filter(item => item.includes("SOUNDCLOUD") | item.includes("SPOTIFY") | item.includes("APPLE") | item.includes("ITUNES"))[0]]
   const linksByPlatform = data.linksByPlatform
 
   const fieldsArray = Object.keys(linksByPlatform).filter(platform => Object.keys(config.platformData).includes(platform)).map(_ => {
@@ -18,9 +18,15 @@ const createEmbed = (data, client) => {
       "url": `${metadata.thumbnailUrl}`
     },
     "footer": {
-      "text": `Service provided by https://song.link ${data.cached ? `(Cached)` : `(Fetched)`}`
+      "text": `Service provided by https://song.link ${data.cached ? `(Cached)` : `(Fetched)`}${Object.keys(fieldsArray).includes('spotify') ? `` : `\n\nThe API seems to of left out spotify\nClick "More" to view all services`}`
     },
-    "fields": fieldsArray
+    "fields": [
+      ...fieldsArray, 
+      {
+        "name": `${config.zeroWidth}`,
+        'value': `🎵 [More](${data.pageUrl})`
+      }
+    ]
   }
 }
 
